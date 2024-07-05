ac999f3df9cba194b0b9d3455f65319d462ba4ca
Ju <aglayrtonjuliao@gmail.com>
2024-07-05 09:24:18 -0300

    Concatenação

---
+++ 1 - fundamentos/concatenacao.php
@@ -0,0 +1,9 @@
+<?php
+
+$primeiraParte = "Olá, ";
+$segundaParte = "mundo!";
+// Resultado: "Olá, mundo!"
+$mensagemCompleta = $primeiraParte . $segundaParte; 
+
+$nome = "Maria";
+$saudacao = "Bem-vinda, " . $nome . "!"; // Resultado: "Bem-vinda, Maria!"
